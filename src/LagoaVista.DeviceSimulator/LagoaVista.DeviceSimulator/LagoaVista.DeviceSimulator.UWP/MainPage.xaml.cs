b341596cc24861ae8ffc9cfdf79f0babab62f257
Kevin D. Wolf <kevinw@software-logistics.com>
2017-03-21 12:41:08 -0400

    initial add

---
+++ src/LagoaVista.DeviceSimulator/LagoaVista.DeviceSimulator/LagoaVista.DeviceSimulator.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LagoaVista.DeviceSimulator.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new LagoaVista.DeviceSimulator.App());
+        }
+    }
+}
---
+++ src/LagoaVista.DeviceSimulator/LagoaVista.DeviceSimulator/LagoaVista.DeviceSimulator/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace LagoaVista.DeviceSimulator.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
